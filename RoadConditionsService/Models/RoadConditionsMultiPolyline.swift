6a86f7a7d80f58f659c19817869dee17c4cbe5f2
Jake Holland <jakeholland@me.com>
2020-01-28 22:00:53 -0600

    Account for geojson returning multipolylines

--- RoadConditionsService/Models/RoadConditionsMultiPolyline.swift
+++ RoadConditionsService/Models/RoadConditionsMultiPolyline.swift
@@ -4,16 +4,22 @@ public final class RoadConditionsMultiPolyline: MKMultiPolyline {
     public let roadConditions: RoadConditions
     
     init?(_ geoJsonFeature: MKGeoJSONFeature) {
-        let polylines = geoJsonFeature.geometry.compactMap { $0 as? MKPolyline }
         guard
-            !polylines.isEmpty,
             let properties = geoJsonFeature.properties,
             let conditions = try? JSONDecoder().decode(IllinoisWinterRoadConditionsResponse.self, from: properties),
             let roadConditionsString = conditions.Cond_Desc,
             let roadConditions = RoadConditions(rawValue: roadConditionsString)
             else { return nil }
-        
         self.roadConditions = roadConditions
+        
+        let polylines = geoJsonFeature.geometry.compactMap { $0 as? MKPolyline }
+        guard !polylines.isEmpty else {
+            let multiPolylines = geoJsonFeature.geometry.compactMap { $0 as? MKMultiPolyline }
+            let polylines = multiPolylines.flatMap { $0.polylines }
+            super.init(polylines)
+            return
+        }
+        
         super.init(polylines)
     }
 }
