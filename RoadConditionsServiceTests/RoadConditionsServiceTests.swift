6b1439ac16f0e5f6131ac0273b776e66b35f5c87
Jake Holland <jakeholland@me.com>
2020-02-24 21:11:14 -0600

    Create basic json fixtures

--- RoadConditionsServiceTests/RoadConditionsServiceTests.swift
+++ RoadConditionsServiceTests/RoadConditionsServiceTests.swift
@@ -19,19 +19,25 @@ final class RoadConditionsServiceTests: HttpMockingTestCase {
                 return
             }
             
-            XCTAssertEqual(roadConditionSegments.count, 2)
+            XCTAssertEqual(roadConditionSegments.count, 3)
             
             XCTAssertEqual(roadConditionSegments[0].roadConditions, RoadConditions.clear)
-            XCTAssertEqual(roadConditionSegments[0].polylines.count, 7332)
+            XCTAssertEqual(roadConditionSegments[0].polylines.count, 307)
             let firstClearPolylineCoordinate = roadConditionSegments[0].polylines[0].coordinate
             XCTAssertEqual(firstClearPolylineCoordinate.latitude, 37.592, accuracy: 0.001)
             XCTAssertEqual(firstClearPolylineCoordinate.longitude, -92.137, accuracy: 0.001)
             
             XCTAssertEqual(roadConditionSegments[1].roadConditions, RoadConditions.partlyCovered)
-            XCTAssertEqual(roadConditionSegments[1].polylines.count, 38)
+            XCTAssertEqual(roadConditionSegments[1].polylines.count, 2)
             let firstPartlyCoveredPolylineCoordinate = roadConditionSegments[1].polylines[0].coordinate
-            XCTAssertEqual(firstPartlyCoveredPolylineCoordinate.latitude, 42.396, accuracy: 0.001)
-            XCTAssertEqual(firstPartlyCoveredPolylineCoordinate.longitude, -98.451, accuracy: 0.001)
+            XCTAssertEqual(firstPartlyCoveredPolylineCoordinate.latitude, 37.971, accuracy: 0.001)
+            XCTAssertEqual(firstPartlyCoveredPolylineCoordinate.longitude, -91.695, accuracy: 0.001)
+            
+            XCTAssertEqual(roadConditionSegments[2].roadConditions, RoadConditions.travelNotAdvised)
+            XCTAssertEqual(roadConditionSegments[2].polylines.count, 1)
+            let firstTravelNotAdvisedPolylineCoordinate = roadConditionSegments[2].polylines[0].coordinate
+            XCTAssertEqual(firstTravelNotAdvisedPolylineCoordinate.latitude, 38.948, accuracy: 0.001)
+            XCTAssertEqual(firstTravelNotAdvisedPolylineCoordinate.longitude, -92.036, accuracy: 0.001)
         }
         
         wait(for: [expectation], timeout: 5)
